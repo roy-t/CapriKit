a70863c8928bc23f7d4c3bef680f398966480b6b
Roy Triesscheijn <roy-t@hotmail.com>
2025-07-27 12:09:44 +0200

    wip: clean-up of generator code

---
+++ source/CapriKit.CommandLine/FlagProperty.cs
@@ -0,0 +1,19 @@
+namespace CapriKit.CommandLine;
+
+internal sealed class FlagProperty
+{
+    public FlagProperty(string propertyType, string propertyName, string parentTypeName, string flagName, string documentation)
+    {
+        PropertyType = propertyType;
+        PropertyName = propertyName;
+        ParentTypeName = parentTypeName;
+        FlagName = flagName;        
+        Documentation = documentation;        
+    }
+
+    public string PropertyType { get; }
+    public string PropertyName { get; }
+    public string ParentTypeName { get; }
+    public string FlagName { get; }   
+    public string Documentation { get; }        
+}
--- source/CapriKit.CommandLine/VerbGenerator.cs
+++ source/CapriKit.CommandLine/VerbGenerator.cs
@@ -1,95 +1,56 @@
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
 using System.Collections.Immutable;
 using System.Diagnostics;
-using System.Reflection;
 using System.Text;
-using System.Xml;
 
 namespace CapriKit.CommandLine;
-
-internal sealed class VerbClass
-{
-    public VerbClass(string name, string type, string documentation, string @namespace)
-    {
-        Name = name;
-        Type = type;
-        Documentation = documentation;
-        Namespace = @namespace;
-    }
-
-    public string Name { get; }
-    public string Type { get; }
-    public string Documentation { get; }
-    public string Namespace { get; }
-}
-
-internal sealed class FlagProperty
-{
-    public FlagProperty(string type, string fieldName, string flagName, string documentation, string parentClass)
-    {
-        Type = type;
-        FieldName = fieldName;
-        FlagName = flagName;        
-        Documentation = documentation;
-        ParentClass = parentClass;        
-    }
-
-    public string Type { get; }
-    public string FieldName { get; }
-    public string FlagName { get; }   
-    public string Documentation { get; }
-    public string ParentClass { get; }
-    
-}
 
 [Generator]
 public class VerbGenerator : IIncrementalGenerator
 {
-    private static string VerbAttributeFullName = "CapriKit.CommandLine.Types.VerbAttribute";
-    private static string VerbAttributeName = "VerbAttribute";
+    private static readonly string VerbAttributeFullName = "CapriKit.CommandLine.Types.VerbAttribute";
+    private static readonly string VerbAttributeName = "VerbAttribute";
 
-    private static string FlagAttributeFullName = "CapriKit.CommandLine.Types.FlagAttribute";
-    private static string FlagAttributeName = "FlagAttribute";
+    private static readonly string FlagAttributeFullName = "CapriKit.CommandLine.Types.FlagAttribute";
+    private static readonly string FlagAttributeName = "FlagAttribute";
+
     public void Initialize(IncrementalGeneratorInitializationContext context)
     {
+        // Get all classes annotated with [Verb(..)]
         var verbDeclarations = context.SyntaxProvider.ForAttributeWithMetadataName(
             VerbAttributeFullName,
             static (node, _) => node is ClassDeclarationSyntax,
             static (ctx, ct) => GetSemanticTargetForVerbGeneration(ctx, ct))
             .Collect();
 
+        // Get all properties annotated with [Flag(..)], even if they are not in class with annotated with [Verb(..)]
         var flagDeclarations = context.SyntaxProvider.ForAttributeWithMetadataName(
             FlagAttributeFullName,
             static (node, _) => node is PropertyDeclarationSyntax,
             static (ctx, ct) => GetSemanticTargetForFlagGeneration(ctx, ct))
             .Collect();
 
+        // Generate codes based on all verb classes and flag properties
         var verbsAndFlags = verbDeclarations.Combine(flagDeclarations);
         context.RegisterSourceOutput(verbsAndFlags, static (spc, source) => Execute(spc, source));
     }
 
-    private static void Execute(SourceProductionContext context, (ImmutableArray<(ClassDeclarationSyntax? Syntax, AttributeData? Data, string? Namespace)> VerbDeclarations, ImmutableArray<(PropertyDeclarationSyntax? Syntax, AttributeData? Data, string? ParentClass)> FlagDeclarations) source)
+    private static void Execute(SourceProductionContext context, (ImmutableArray<(ClassDeclarationSyntax Syntax, AttributeData Data, string Namespace)> VerbDeclarations, ImmutableArray<(PropertyDeclarationSyntax Syntax, AttributeData Data, string ParentClass)> FlagDeclarations) source)
     {
         var flags = new List<FlagProperty>();
         foreach (var flagDeclaration in source.FlagDeclarations)
         {
             var flag = GetFlag(flagDeclaration.Syntax, flagDeclaration.Data, flagDeclaration.ParentClass);
-            if (flag != null)
-            {
-                flags.Add(flag);
-            }
+            flags.Add(flag);            
         }
 
         var verbs = new List<VerbClass>();
         foreach(var verbDeclaration in source.VerbDeclarations)
         {
             var verb = GetVerb(verbDeclaration.Syntax, verbDeclaration.Data, verbDeclaration.Namespace);
-            if (verb != null)
-            {
-                verbs.Add(verb);
-            }
+            verbs.Add(verb);
         }
 
         GenerateCode(context, verbs, flags);
@@ -100,56 +61,54 @@ public class VerbGenerator : IIncrementalGenerator
         foreach (var verb in verbs)
         {
             var builder = new StringBuilder();
-            builder.AppendLine($"namespace {verb.Namespace};");
-            builder.AppendLine($"public partial class {verb.Type}");
+            builder.AppendLine($"namespace {verb.TypeNamespace};");
+            builder.AppendLine($"public partial class {verb.TypeName}");
             builder.AppendLine("{");
 
             foreach (var flag in flags)
             {
-                if (flag.ParentClass != verb.Type)
+                if (flag.ParentTypeName != verb.TypeName)
                 {
                     continue;
                 }
 
-                builder.AppendLine($"  private {flag.Type} _{flag.FieldName} = default;");
-                builder.AppendLine($"  public partial {flag.Type} {flag.FieldName} {{ get => _{flag.FieldName}; }}");
+                builder.AppendLine($"  private {flag.PropertyType} _{flag.PropertyName} = default;");
+                builder.AppendLine($"  public partial {flag.PropertyType} {flag.PropertyName} {{ get => _{flag.PropertyName}; }}");
             }
             // TODO this generates the partial classes but not the parsing stuff!
 
             builder.AppendLine("}");
-            context.AddSource($"VerbGenerator.{verb.Namespace}.{verb.Type}.g.cs", builder.ToString());
+            context.AddSource($"VerbGenerator.{verb.TypeNamespace}.{verb.TypeName}.g.cs", builder.ToString());
         }               
     }
 
-    private static VerbClass? GetVerb(ClassDeclarationSyntax? syntax, AttributeData? data, string? @namespace)
-    {
-        if (syntax == null || data == null || @namespace == null)
-        {
-            return null;
-        }
-
+    private static VerbClass GetVerb(ClassDeclarationSyntax syntax, AttributeData data, string @namespace)
+    {        
         if (data.ConstructorArguments.Length != 1)
         {
             throw new Exception($"{VerbAttributeFullName} should have exactly one constructor argument");
         }
 
-        var name = data.ConstructorArguments[0].Value?.ToString() ?? string.Empty;
-        var type = syntax.Identifier.ValueText ?? string.Empty;        
-        var documentation = GetDocumentationFromLeadingTrivia(syntax);
+        var typeName = syntax.Identifier.ValueText;
+        var verbName = data.ConstructorArguments[0].Value?.ToString() ?? string.Empty;        
+        var documentation = Utilities.GetDocumentationFromLeadingTrivia(syntax);
 
-        return new VerbClass(name, type, documentation, @namespace);
+        return new VerbClass(typeName, @namespace, verbName, documentation);
     }
 
-    private static FlagProperty? GetFlag(PropertyDeclarationSyntax? syntax, AttributeData? data, string? parentClass)
-    {
-        if (syntax == null || data == null || parentClass == null)
-        {
-            return null;
-        }
-
+    private static FlagProperty GetFlag(PropertyDeclarationSyntax syntax, AttributeData data, string parentTypeName)
+    {        
+        // Limit support to built-in-types: https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/built-in-types        
         if (syntax.Type is not PredefinedTypeSyntax predefinedType)
         {
-            return null;
+            throw new NotSupportedException($"The flag attribute can only be used on built in types");
+        }
+
+        // Ignore built-in reference types, except for string
+        var propertyType = predefinedType.Keyword.ValueText;
+        if (propertyType is "object" or "delegate" or "dynamic")
+        {
+            throw new NotSupportedException($"The flag attribute cannot be used on 'object', 'delegate, or 'dynamic' types");
         }
 
         if (data.ConstructorArguments.Length != 1)
@@ -157,146 +116,54 @@ public class VerbGenerator : IIncrementalGenerator
             throw new Exception($"{FlagAttributeFullName} should have exactly one constructor argument");
         }        
 
-        var name = data.ConstructorArguments[0].Value?.ToString() ?? string.Empty;
-        var fieldName = syntax.Identifier.ValueText ?? string.Empty;
-        var type = predefinedType.Keyword.ValueText;
-        var documentation = GetDocumentationFromLeadingTrivia(syntax);       
-
-        return new FlagProperty(type, fieldName, name, documentation, parentClass);
-    }
-
-    private static string GetDocumentationFromLeadingTrivia(SyntaxNode syntax)
-    {
-        if(syntax.HasLeadingTrivia)
-        {
-            var trivia = syntax.GetLeadingTrivia();
-            foreach(var triviaNode in trivia)
-            {
-                var kind = triviaNode.Kind();
-                if (kind == SyntaxKind.SingleLineDocumentationCommentTrivia)
-                {
-                    var comment = triviaNode.ToFullString();
-                    return GetDocumentationFromXmlComment(comment);
-                }                
-            }
-        }
-
-        return string.Empty;
-    }
-
-    private static string GetDocumentationFromXmlComment(string comment)
-    {
-        try
-        {
-            var lines = comment.Split(['\r', '\n'], StringSplitOptions.RemoveEmptyEntries);
-            var builder = new StringBuilder();
-            foreach (var line in lines)
-            {
-                if (line.StartsWith("///"))
-                {
-                    if (line.Length > 3)
-                    {
-                        builder.AppendLine(line.Substring(3).Trim());
-                    }
-                    else
-                    {
-                        builder.AppendLine();
-                    }
-                }
-            }
-
-            var xml = builder.ToString();
-
-            var settings = new XmlReaderSettings()
-            {
-                ConformanceLevel = ConformanceLevel.Fragment
-            };
-            using var reader = XmlReader.Create(new StringReader(xml), settings);
-            while (reader.Read())
-            {
-                if (reader.NodeType == XmlNodeType.Element && reader.Name == "summary")
-                {
-                    return reader.ReadInnerXml();
-                }
-            }
-        }
-        catch (Exception)
-        {
-            // ignored
-        }
+        var flagName = data.ConstructorArguments[0].Value?.ToString() ?? string.Empty;
+        var propertyName = syntax.Identifier.ValueText ?? string.Empty;
         
-        return string.Empty;
+        var documentation = Utilities.GetDocumentationFromLeadingTrivia(syntax);       
+
+        return new FlagProperty(propertyType, propertyName, parentTypeName, flagName, documentation);
+    }
+    
+    private static (ClassDeclarationSyntax Syntax, AttributeData Data, string Namespace) GetSemanticTargetForVerbGeneration(GeneratorAttributeSyntaxContext context, CancellationToken ct)
+    {
+        if (context.TargetNode is not ClassDeclarationSyntax classDeclaration)
+        {
+            throw new NotSupportedException($"Target of [{VerbAttributeFullName}] should be a class");
+        }
+
+        // Technically the syntax provider should never give us a node without the attribute we requested
+        var attribute = context.Attributes.FirstOrDefault(a => a.AttributeClass?.Name == VerbAttributeName);
+        if (attribute == null)
+        {
+            throw new Exception($"Could not find attribute");
+        }
+
+        var ns = Utilities.GetNamespace(classDeclaration);
+
+        return (classDeclaration, attribute, ns);
+    }
+
+    private static (PropertyDeclarationSyntax Syntax, AttributeData Data, string ParentClass) GetSemanticTargetForFlagGeneration(GeneratorAttributeSyntaxContext context, CancellationToken ct)
+    {
+        if (context.TargetNode is not PropertyDeclarationSyntax properyDeclaration)
+        {
+            throw new NotSupportedException($"Target of [{FlagAttributeFullName}] should be a property");
+        }
+
+        // Technically the syntax provider should never give us a node without the attribute we requested
+        var attribute = context.Attributes.First(a => a.AttributeClass?.Name == FlagAttributeName);
+        if (attribute == null)
+        {
+            throw new Exception($"Could not find attribute");
+        }
+
+        // This can happen if the property is defined on a struct, which we currently do not supported
+        var parentClass = context.TargetNode.FirstAncestorOrSelf<ClassDeclarationSyntax>()?.Identifier.ValueText;
+        if (parentClass == null)
+        {
+            throw new NotSupportedException($"Properties attributed with [{FlagAttributeFullName}] should be defined in a class");
+        }
+
+        return (properyDeclaration, attribute, parentClass);        
     }    
-
-    private static (ClassDeclarationSyntax? Syntax, AttributeData? Data, string? Namespace) GetSemanticTargetForVerbGeneration(GeneratorAttributeSyntaxContext context, CancellationToken ct)
-    {
-        if (context.TargetNode is ClassDeclarationSyntax classDeclaration)
-        {
-            var attribute = context.Attributes.FirstOrDefault(a => a.AttributeClass?.Name == VerbAttributeName);
-            var ns = GetNamespace(classDeclaration);
-            return (classDeclaration, attribute, ns);
-        }
-
-        return (null, null, null);
-    }
-
-    private static (PropertyDeclarationSyntax? Syntax, AttributeData? Data, string? ParentClass) GetSemanticTargetForFlagGeneration(GeneratorAttributeSyntaxContext context, CancellationToken ct)
-    {
-        if (context.TargetNode is PropertyDeclarationSyntax properyDeclarationSyntax)
-        {            
-
-            var attribute = context.Attributes.FirstOrDefault(a => a.AttributeClass?.Name == FlagAttributeName);
-            var parentClass = context.TargetNode.FirstAncestorOrSelf<ClassDeclarationSyntax>()
-                ?.Identifier.ValueText ?? string.Empty;            
-            return (properyDeclarationSyntax, attribute, parentClass);
-        }
-
-        return (null, null, null);
-    }
-
-    // from: https://andrewlock.net/creating-a-source-generator-part-5-finding-a-type-declarations-namespace-and-type-hierarchy/
-    // determine the namespace the class/enum/struct is declared in, if any
-    static string GetNamespace(BaseTypeDeclarationSyntax syntax)
-    {
-        // If we don't have a namespace at all we'll return an empty string
-        // This accounts for the "default namespace" case
-        string nameSpace = string.Empty;
-
-        // Get the containing syntax node for the type declaration
-        // (could be a nested type, for example)
-        SyntaxNode? potentialNamespaceParent = syntax.Parent;
-
-        // Keep moving "out" of nested classes etc until we get to a namespace
-        // or until we run out of parents
-        while (potentialNamespaceParent != null &&
-                potentialNamespaceParent is not NamespaceDeclarationSyntax
-                && potentialNamespaceParent is not FileScopedNamespaceDeclarationSyntax)
-        {
-            potentialNamespaceParent = potentialNamespaceParent.Parent;
-        }
-
-        // Build up the final namespace by looping until we no longer have a namespace declaration
-        if (potentialNamespaceParent is BaseNamespaceDeclarationSyntax namespaceParent)
-        {
-            // We have a namespace. Use that as the type
-            nameSpace = namespaceParent.Name.ToString();
-
-            // Keep moving "out" of the namespace declarations until we 
-            // run out of nested namespace declarations
-            while (true)
-            {
-                if (namespaceParent.Parent is not NamespaceDeclarationSyntax parent)
-                {
-                    break;
-                }
-
-                // Add the outer namespace as a prefix to the final namespace
-                nameSpace = $"{namespaceParent.Name}.{nameSpace}";
-                namespaceParent = parent;
-            }
-        }
-
-        // return the final namespace
-        return nameSpace;
-    }
 }
