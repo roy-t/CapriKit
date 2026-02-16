822135172afbc137d1ebe4ae8f33c59a2fa18c02
Roy Triesscheijn <roy-t@hotmail.com>
2026-02-16 17:14:35 +0100

    WIP ImGui

---
+++ source/CapriKit.DirectX11/Debug/ImGuiController.cs
@@ -0,0 +1,47 @@
+using CapriKit.Win32;
+using CapriKit.Win32.Input;
+using ImGuiNET;
+using System.Numerics;
+
+namespace CapriKit.DirectX11.Debug;
+
+public sealed class ImGuiController : IDisposable
+{
+    private readonly ImGuiIOPtr IO;
+    private readonly ImGuiRenderer Renderer;
+    private readonly ImGuiInput Input;
+
+    public ImGuiController(Device device, Win32Window window, Keyboard keyboard, Mouse mouse)
+    {
+        ImGui.CreateContext();
+        IO = ImGui.GetIO();
+        Renderer = new ImGuiRenderer(device);
+        Input = new ImGuiInput(IO, window, keyboard, mouse);
+
+        this.Resize(device.Width, device.Height);
+    }
+
+    public void Resize(int width, int height)
+    {
+        IO.DisplaySize = new Vector2(width, height);
+    }
+
+    public void NewFrame(float elapsed)
+    {
+        IO.DeltaTime = elapsed;
+        Input.NewFrame();
+        ImGui.NewFrame();
+    }
+
+    public void Render()
+    {
+        ImGui.Render();
+        Renderer.Render(ImGui.GetDrawData());
+    }
+
+    public void Dispose()
+    {
+        Renderer.Dispose();
+        ImGui.DestroyContext();
+    }
+}
---
+++ source/CapriKit.DirectX11/Debug/ImGuiRenderer.cs
@@ -0,0 +1,28 @@
+using ImGuiNET;
+
+namespace CapriKit.DirectX11.Debug;
+
+// TODO: For IMGUI I should probably just render on the ImmediateContext as its a debug view
+// for other rendering Tasks is probably not a good idea as I can't really await anything and
+// I need to task.Run(..) and have overhead. Instead I should come up with something similar like I did
+// in the old engine, but with better ergonomics. Like A DirectX11Job system that immediately starts work when
+// its enqueued, separate from the simulation.
+public sealed class ImGuiRenderer : IDisposable
+{
+    private readonly Device Device;
+
+    public ImGuiRenderer(Device device)
+    {
+        this.Device = device;
+    }
+
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal void Render(ImDrawDataPtr imDrawDataPtr)
+    {
+        throw new NotImplementedException();
+    }
+}
