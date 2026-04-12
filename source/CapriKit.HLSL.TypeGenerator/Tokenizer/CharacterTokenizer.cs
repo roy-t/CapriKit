da45a1eef2867cdc2f31520e19b846ffe31fc9da
Roy Triesscheijn <roy-t@hotmail.com>
2026-04-12 21:22:48 +0200

    Explain character tokenizer limitations

--- source/CapriKit.HLSL.TypeGenerator/Tokenizer/CharacterTokenizer.cs
+++ source/CapriKit.HLSL.TypeGenerator/Tokenizer/CharacterTokenizer.cs
@@ -10,12 +10,23 @@ public static class CharacterTokenizer
     /// <seealso href="https://learn.microsoft.com/en-us/windows/win32/direct3dhlsl/dx-graphics-hlsl-appendix-grammar#characters"/>
     public static int ReadCharacter(string source, int offset, List<Token> tokens)
     {
+        // TODO: this method accepts any string of characters as a character literal
+        // while only escape sequences like '\n' '\101' (octal for 'A') and '\x51' (hex for 'A').
+        // can lead to character literals that span more than 1 character.
+
+        // TODO: this code is equivalent to strings right now
         var cursor = offset;
         if (TryPeek(source, cursor, out var start) && start == '\'')
         {
+            
             cursor++;
-            while (TryPeek(source, cursor, out var c) && (c == '\\' || char.IsLetterOrDigit(c)))
+            while (TryPeek(source, cursor, out var c) && c != '\"')
             {
+                // skip escaped characters so that we don't stop at \"
+                if (c == '\\')
+                {
+                    cursor++;
+                }
                 cursor++;
             }
 
@@ -23,9 +34,8 @@ public static class CharacterTokenizer
             {
                 cursor++;
                 var length = cursor - offset;
-                tokens.Add(new Token(source, offset, length, TokenKind.Character));
+                tokens.Add(new Token(source, offset, length, TokenKind.String));
                 return length;
-
             }
         }
 
