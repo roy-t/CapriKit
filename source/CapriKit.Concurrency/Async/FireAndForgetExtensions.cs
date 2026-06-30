16844d4a37eb4bead1af83aef10955de69e84f56
Roy Triesscheijn <roy-t@hotmail.com>
2026-06-30 21:34:07 +0200

    WIP fire and forget #2

---
+++ source/CapriKit.Concurrency/Async/FireAndForgetExtensions.cs
@@ -0,0 +1,61 @@
+using System.Runtime.CompilerServices;
+
+namespace CapriKit.Concurrency.Async;
+
+public static class FireAndForgetExtensions
+{
+    /// <summary>
+    /// Fires and forgets a task while safely observing any exceptions
+    /// </summary>
+    public static void FireAndForget(
+        this Task task,
+        Action<Exception> onException,
+        [CallerMemberName] string member = "",
+        [CallerFilePath] string file = "",
+        [CallerLineNumber] int line = 0)
+    {
+        _ = AwaitAndCatch(task, onException, member, file, line);
+    }
+
+    private static async Task AwaitAndCatch(
+        Task task,
+        Action<Exception> onException,
+        string member,
+        string file,
+        int line)
+    {
+        if (task.IsCompletedSuccessfully)
+        {
+            return;
+        }
+
+        try
+        {
+            await task.ConfigureAwait(false);
+        }
+        catch (OperationCanceledException)
+        {
+            // Ignore cooperative cancellations
+        }
+        catch (Exception ex)
+        {
+            // TODO: does this make sense, does this break the stacktrace
+            onException(new FaFTaskException(ex, member, file, line));
+        }
+    }
+}
+
+public sealed class FaFTaskException : Exception
+{
+    public FaFTaskException(Exception innerException, string member, string file, int line)
+        : base($"A fire-and-forget task started from '{member}' at {file}:{line} failed.", innerException)
+    {
+        Member = member;
+        File = file;
+        Line = line;
+    }
+
+    public string Member { get; }
+    public string File { get; }
+    public int Line { get; }
+}
--- source/CapriKit.Tests.Tool/WorkerConsumer.cs
+++ source/CapriKit.Tests.Tool/WorkerConsumer.cs
@@ -1,5 +1,7 @@
+using CapriKit.Concurrency.Async;
 using CapriKit.Tests.Tool.Tests.Framework;
 using System.Collections.Concurrent;
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Threading.Channels;
 
@@ -42,7 +44,7 @@ internal sealed class TestScreenLoader
             {
                 await Work.Writer.WriteAsync(new Loaded<ITestScreen>(job.Id, null, ex));
             }
-        }).Forget();
+        }).FireAndForget(ex => Debugger.Log(1, "Task", ex.Message));
         // Or should this just be static and return a channel?
         // No need for BlockingCollection now.
     }
@@ -72,39 +74,4 @@ internal sealed class TestScreenLoader
     //{
     //    return new LoadedShader([]);
     //}
-}
-
-
-public static class TaskExtensions
-{
-    /// <summary>
-    /// Observes the task to avoid the UnobservedTaskException event to be raised.
-    /// </summary>
-    public static void Forget(this Task task)
-    {
-        // note: this code is inspired by a tweet from Ben Adams: https://twitter.com/ben_a_adams/status/1045060828700037125
-        // Only care about tasks that may fault (not completed) or are faulted,
-        // so fast-path for SuccessfullyCompleted and Canceled tasks.
-        if (!task.IsCompleted || task.IsFaulted)
-        {
-            // use "_" (Discard operation) to remove the warning IDE0058: Because this call is not awaited, execution of the current method continues before the call is completed
-            // https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/functional/discards?WT.mc_id=DT-MVP-5003978#a-standalone-discard
-            _ = ForgetAwaited(task);
-        }
-
-        // Allocate the async/await state machine only when needed for performance reasons.
-        // More info about the state machine: https://blogs.msdn.microsoft.com/seteplia/2017/11/30/dissecting-the-async-methods-in-c/?WT.mc_id=DT-MVP-5003978
-        async static Task ForgetAwaited(Task task)
-        {
-            try
-            {
-                // No need to resume on the original SynchronizationContext, so use ConfigureAwait(false)
-                await task.ConfigureAwait(false);
-            }
-            catch
-            {
-                // Nothing to do here
-            }
-        }
-    }
 }
