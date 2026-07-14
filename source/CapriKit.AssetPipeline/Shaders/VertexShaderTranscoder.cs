c177ba516525ccb6e507ffb2ddd216b03913479c
Roy Triesscheijn <roy-t@hotmail.com>
2026-07-14 21:16:13 +0200

    Move dependency towards AssetPipeline project

---
+++ source/CapriKit.AssetPipeline/Shaders/VertexShaderTranscoder.cs
@@ -0,0 +1,32 @@
+using CapriKit.DirectX11;
+using CapriKit.DirectX11.Resources.Shaders;
+using CapriKit.IO;
+using System.Buffers;
+
+namespace CapriKit.AssetPipeline.Shaders;
+
+internal sealed class VertexShaderTranscoder(Device device) : IAssetEncoder, IAssetDecoder<IVertexShader>
+{
+    public IReadOnlySet<string> SupportedExtensions => ShaderTranscoder.SupportedExtensions;
+    public Guid Id => Guid.Parse("{CA3CB37D-9880-4B61-AB09-EBC17E7533E6}");
+    public int Version => 1;
+
+    public async Task Encode(AssetId id, IReadOnlyVirtualFileSystem fileSystem, IBufferWriter<byte> writer)
+    {
+        var source = await fileSystem.ReadAllText(id.Path);
+        var includePath = id.Path.Directory;
+        var bytes = ShaderCompiler.CompileVertexShader(fileSystem, includePath, source, id.Key, id.ToString());
+        ShaderTranscoder.WriteCommon(bytes.Common, writer);
+    }
+
+    public IVertexShader Decode(AssetId id, ref SequenceReader<byte> reader)
+    {
+        var common = ShaderTranscoder.ReadCommon(ref reader);
+        return ShaderCompiler.CreateVertexShader(new VertexShaderByteCode(common), device);
+    }
+
+    public void HotSwap(IVertexShader instance, IVertexShader replacement)
+    {
+        instance.HotSwap(replacement);
+    }
+}
--- source/CapriKit.DirectX11/Resources/Shaders/VertexShader.cs
+++ source/CapriKit.DirectX11/Resources/Shaders/VertexShader.cs
@@ -4,36 +4,52 @@ namespace CapriKit.DirectX11.Resources.Shaders;
 
 public interface IVertexShader : IDisposable
 {
+    internal byte[] Blob { get; set; }
     internal ID3D11VertexShader ID3D11VertexShader { get; set; }
 
     IInputLayout CreateInputLayout(Device device, InputElementDescription[] elements);
+
+    public void HotSwap(IVertexShader replacement)
+    {
+        Blob = replacement.Blob;
+
+        var oldShader = ID3D11VertexShader;
+        ID3D11VertexShader = replacement.ID3D11VertexShader;
+        oldShader.Dispose();
+    }
 }
 
 internal sealed class VertexShader : IVertexShader
 {
-    private readonly byte[] Blob;
-    private ID3D11VertexShader Shader;
+    private byte[] blob;
+    private ID3D11VertexShader shader;
 
     internal VertexShader(byte[] blob, ID3D11VertexShader shader)
     {
-        Blob = blob;
-        Shader = shader;
+        this.blob = blob;
+        this.shader = shader;
+    }
+
+    byte[] IVertexShader.Blob
+    {
+        get { return blob; }
+        set { blob = value; }
     }
 
     ID3D11VertexShader IVertexShader.ID3D11VertexShader
     {
-        get { return Shader; }
-        set { Shader = value; }
+        get { return shader; }
+        set { shader = value; }
     }
 
     public IInputLayout CreateInputLayout(Device device, InputElementDescription[] elements)
     {
-        var inputLayout = device.ID3D11Device.CreateInputLayout(elements, Blob);
+        var inputLayout = device.ID3D11Device.CreateInputLayout(elements, blob);
         return new InputLayout(inputLayout);
     }
 
     public void Dispose()
     {
-        Shader.Dispose();
+        shader.Dispose();
     }
 }
